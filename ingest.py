4596c26522ff226fec3e8ebf23488ac1d9cffb92
Mike Powers <72277454+Aphelious@users.noreply.github.com>
2022-05-05 20:58:39 -0400

    Refactor and add ingest_yellow_data()

--- ingest.py
+++ ingest.py
@@ -4,21 +4,20 @@ from sqlalchemy import create_engine
 engine = create_engine('postgresql://root:root@localhost:5432/ny_taxi')
 engine.connect()
 
-df = pd.read_csv('/Users/mike/Desktop/Main/Programming/Projects/Tutorials/Datazoomcamp/week1/2022_jan_green.csv')
-# df_iter = pd.read_csv('/Users/mike/Desktop/Main/Programming/Projects/Tutorials/Datazoomcamp/week1/2022_jan_yellow.csv', iterator=True, chunksize=100000)
-# df = next(df_iter)
-# print(df.info())
 
-# while True:
-#     df = next(df_iter)
-#
-#     df["tpep_pickup_datetime"] = pd.to_datetime(df["tpep_pickup_datetime"])
-#     df["tpep_dropoff_datetime"] = pd.to_datetime(df["tpep_dropoff_datetime"])
-#
-#     df.to_sql(name='yellow_taxi_data', con=engine, if_exists='append')
-#     print("Inserted another chunk...")
+def ingest_green_data():
+    df_iter = pd.read_csv('/Users/mike/Desktop/Main/Programming/Projects/Tutorials/Datazoomcamp/week1/2022_jan_yellow.csv', iterator=True, chunksize=50000)
 
-# print(pd.io.sql.get_schema(df, name='yellow_taxi_data', con=engine))
+    while True:
+        df = next(df_iter)
+
+        df["tpep_pickup_datetime"] = pd.to_datetime(df["tpep_pickup_datetime"])
+        df["tpep_dropoff_datetime"] = pd.to_datetime(df["tpep_dropoff_datetime"])
+
+        df.to_sql(name='yellow_taxi_data', con=engine, if_exists='append')
+        print("Inserted another chunk...")
+
+
 def ingest_green_data():
     df = pd.read_csv('/Users/mike/Desktop/Main/Programming/Projects/Tutorials/Datazoomcamp/week1/2022_jan_green.csv')
 
